a1a77a8fc3eeaa290a3f8d4bc849ed1554c6b66a
jaechan <penguin_03@naver.com>
2024-10-02 03:37:47 +0900

    feat: 깃헙 어세스토큰 저장, 가져오기  로직 구현

---
+++ github/githubStorage.js
@@ -0,0 +1,20 @@
+export async function getToken() {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(["githubAccessToken"], (result) => {
+      if (chrome.runtime.lastError) {
+        return reject(new Error(chrome.runtime.lastError));
+      }
+      resolve(result.accessToken);
+    });
+  });
+}
+
+export async function saveToken(accessToken) {
+  chrome.storage.local.set({ githubAccessToken: accessToken }, () => {
+    if (chrome.runtime.lastError) {
+      console.error("저장하는데 문제 발생:", chrome.runtime.lastError);
+    } else {
+      console.log("성공적으로 저장되었습니다.");
+    }
+  });
+}
